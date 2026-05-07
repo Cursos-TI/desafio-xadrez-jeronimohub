14696a8f766a5b7e244bb8d968edc346f041cd4c
jeronimohub <jeronimosantosmelo13@gmail.com>
2026-05-07 18:49:47 +0000

    Criei um do-while para o movimento da peça torre.

--- xadrez.c
+++ xadrez.c
@@ -7,7 +7,7 @@
 int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
-    int b = 1, t, r;
+    int b = 1, t = 1, r;
 
     // Menu interativo
     printf("*** Jogo de tabuleiro de xadrez *** \n");
@@ -19,18 +19,22 @@ int main() {
     while (b <= 5)
     {
        printf("Cima direita\n");
-       b++; 
-       
+       b++;  
     }
 
     // Implementação de Movimentação da Torre
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita (5 casas para a direita).
-
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita (5 casas para a direita).
-
+    printf("Movimento da torre\n");
+    do
+    {
+       printf("Direita\n");
+       t++;
+    } while (t <= 5);
+    
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda (Rainha: 8 casas para a esquerda).
+    printf("Movimento da rainha\n");
+    
 
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
